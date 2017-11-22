e4c7813c722e402e556c749cacd9a2c2fbd7e568
Tristan Gemus <tristangemus@gmail.com>
2017-11-22 07:12:09 -0500

    Add basic scripts like lighting, controls, etc, more assets

--- public/examples/background-image/scripts.js
+++ public/examples/background-image/scripts.js
@@ -1,16 +1,55 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, 640 / 360, 0.1, 1000 );
+var earthMesh;
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( 640, 360);
+renderer.setSize( 640, 360 );
+
 document.body.appendChild( renderer.domElement );
 
-camera.position.z = 5;
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = true;
+
+var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+keyLight.position.set(-100, 0, 100);
+
+var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+fillLight.position.set(100, 0, 100);
+
+var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+backLight.position.set(100, 0, -100).normalize();
+
+scene.add(keyLight);
+scene.add(fillLight);
+scene.add(backLight);
+
+var manager = new THREE.LoadingManager();
+manager.onLoad = init;
+
+var loader = new THREE.TextureLoader( manager );
+
+loader.load( '/examples/background-image/assets/earth.jpg', function( texture ) {
+	var geometry = new THREE.SphereGeometry(1, 256, 256);
+	var material = new THREE.MeshPhongMaterial();
+	material.map = texture;
+	earthMesh = new THREE.Mesh(geometry, material);
+	scene.add( earthMesh );
+});
+
+camera.position.z = 3;
 
 var animate = function () {
 	requestAnimationFrame( animate );
 
-	renderer.render(scene, camera);
+	controls.update();
+
+	earthMesh.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
 };
 
-animate();
+function init() {
+	animate();
+};
