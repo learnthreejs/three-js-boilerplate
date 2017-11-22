a6a1cb92edf4025352f0dc1282dc758ab1548246
Tristan Gemus <tristangemus@gmail.com>
2017-11-22 06:36:21 -0500

    Setup for background image example

---
+++ public/examples/background-image/scripts.js
@@ -0,0 +1,16 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+camera.position.z = 5;
+
+var animate = function () {
+	requestAnimationFrame( animate );
+
+	renderer.render(scene, camera);
+};
+
+animate();
