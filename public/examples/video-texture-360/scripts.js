6c0e97be12e1058a43c77c4e86bc5d58698129e7
Tristan Gemus <tristangemus@gmail.com>
2017-11-24 07:08:21 -0500

    Add video texture code

--- public/examples/video-texture-360/scripts.js
+++ public/examples/video-texture-360/scripts.js
@@ -10,7 +10,26 @@ var controls = new THREE.OrbitControls( camera );
 camera.position.z = 300;
 
 function init() {
+	var video = document.createElement( 'video' );
+	video.width = window.innerWidth;
+	video.height = window.innerHeight;
+	video.loop = true;
+	video.muted = true;
+	video.src = '/examples/video-texture-360/assets/video.mp4';
+	video.setAttribute( 'webkit-playsinline', 'webkit-playsinline' );
+	video.play();
 
+	var geometry = new THREE.SphereBufferGeometry( 500, 60, 40 );
+	geometry.scale( -1, 1, 1 );
+
+	var texture = new THREE.VideoTexture( video );
+	texture.minFilter = THREE.LinearFilter;
+	texture.format = THREE.RGBFormat;
+
+	var material = new THREE.MeshBasicMaterial( { map: texture } );
+	var mesh = new THREE.Mesh( geometry, material );
+	mesh.rotation.y = -Math.PI / 2;
+	scene.add( mesh );
 }
 
 function animate () {
