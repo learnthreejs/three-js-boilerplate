57752159ac71cebd4f2c383f04acd41bc0e871f0
Tristan Gemus <tristangemus@gmail.com>
2017-11-26 10:05:55 -0500

    Finish draggable spheres example

--- public/examples/draggable-objects-dragcontrols/scripts.js
+++ public/examples/draggable-objects-dragcontrols/scripts.js
@@ -1,14 +1,54 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.background = new THREE.Color( 0xf0f0f0 );
+var camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 10000 );
+var objects = [];
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-camera.position.z = 5;
+camera.position.z = 1000;
+
+var startColor;
 
 function init() {
+	scene.add( new THREE.AmbientLight( 0x0f0f0f ) );
 
+	var light = new THREE.SpotLight( 0xffffff, 1.5 );
+	light.position.set( 0, 500, 2000 );
+
+	scene.add(light);
+
+	var geometry = new THREE.BoxGeometry( 40, 40, 40 );
+	var geometry = new THREE.SphereGeometry( 40, 40, 40 );
+
+	for (var i = 0; i < 100; i++) {
+		var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
+
+		object.position.x = Math.random() * 1000 - 500;
+		object.position.y = Math.random() * 600 - 300;
+		object.position.z = Math.random() * 800 - 400;
+
+		object.castShadow = true;
+		object.receiveShadow = true;
+
+		scene.add( object );
+
+		objects.push( object );
+	}
+
+	var controls = new THREE.DragControls( objects, camera, renderer.domElement );
+	controls.addEventListener( 'dragstart', dragStartCallback );
+	controls.addEventListener( 'dragend', dragendCallback );
+}
+
+function dragStartCallback(event) {
+	startColor = event.object.material.color.getHex();
+	event.object.material.color.setHex(0x000000);
+}
+
+function dragendCallback(event) {
+	event.object.material.color.setHex(startColor);
 }
 
 function animate() {
