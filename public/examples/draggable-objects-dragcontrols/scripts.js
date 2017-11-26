5ba59c2edf779ece6d252b3e5806b81fffa461a3
Tristan Gemus <tristangemus@gmail.com>
2017-11-26 08:50:09 -0500

    Add starter files for draggable example

---
+++ public/examples/draggable-objects-dragcontrols/scripts.js
@@ -0,0 +1,21 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+camera.position.z = 5;
+
+function init() {
+
+}
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	renderer.render(scene, camera);
+};
+
+init();
+animate();
